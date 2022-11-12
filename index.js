08a1b1b19b4d68e4389f7cb1c550cfa64e1e1c84
Robert Cohn <robert.s.cohn@intel.com>
2022-11-12 09:10:46 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+packages = {
+    'tbb': 'https://registrationcenter-download.intel.com/akdlm/irc_nas/19041/l_tbb_oneapi_p_2021.7.1.15005_offline.sh',
+    'dpl': 'https://registrationcenter-download.intel.com/akdlm/irc_nas/19046/l_oneDPL_p_2021.7.2.15007_offline.sh'
+}
+
+function install(component) {
+    console.log(`Installing ${component} from ${packages[component]}`)
+}
+
+function install_list(components) {
+    console.log(`Installing ${components}`)
+    for (const component of components) {
+        install(component)
+    }
+}
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    install_list(core.getMultilineInput('components'));
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
